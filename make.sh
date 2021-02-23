536d87d5425568c73ccdaf03638376e10d5c0f81
yangkunlun <yangkunlun@outlook.com>
2021-02-23 17:08:03 +0800

    update test2.c example

--- make.sh
+++ make.sh
@@ -7,6 +7,6 @@ cd build; rm -rf build; make all; cd ..
 flags="--std=gnu99 -I/home/protium/include/ -L/home/protium/lib"
 links="build/libsu.a -lptcomm"
 gcc $flags test1.c $links -o test1.exe
-gcc --std=gnu99 -I/home/protium/include/ -L/home/protium/lib t2.c build/libsu.a -lptcomm -o t2.exe
+gcc $flags test2.c $links -o test2.exe
 gcc --std=gnu99 -I/home/protium/include/ -L/home/protium/lib t3.c build/libsu.a -lptcomm -o t3.exe
 
---
+++ test2.c
@@ -0,0 +1,34 @@
+#include "libsu.h"
+
+//create a su file from scratch and copy over
+int main()
+{
+    SUID_t su1, su2;
+    su_open(&su1, "model_cp.su", SU_READONLY);
+    int ninst = su_ninst(su1);
+    int nsamp = su_nsamp(su1);
+    printf("ninst=%d, nsamp=%d\n", ninst, nsamp);
+
+    su_open(&su2, "model_test2c.su", SU_CREATE);
+    su_setnsamp(su2, nsamp);
+
+    float *v1 = calloc(nsamp, sizeof(float));
+    float *v2 = calloc(nsamp, sizeof(float));
+    for(int i=0; i<ninst; i++) {
+        su_read(su1, "trace", v1, i, 1);
+        for(int j=0; j<nsamp; j++)
+            v2[j] = -1*v1[j];
+        su_write(su2, "trace", v2, i, 1);
+    }
+    free(v1);
+    free(v2);
+    int32_t *ns = calloc(ninst, sizeof(int32_t));
+    for(int i=0; i<ninst; i++)
+        ns[i] = nsamp;
+    su_write(su2, "ns", ns, 0, ninst);
+    su_close(su1);
+    su_close(su2);
+
+    printf("#Reach success!\n");
+    return 0;
+}
