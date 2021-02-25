448bf4204253169a0d4692f7728b92b83dd8c6c4
yangkunlun <yangkunlun@outlook.com>
2021-02-25 13:46:20 +0800

    remove .nbyte from su_write.c

--- su_write.c
+++ su_write.c
@@ -3,10 +3,11 @@ static int su_writebytes(SUID_t id, const su_attr_t *attr,
 {
     protium_suid_t *su = id;
     assert(su->skip!=0 && su->ninst>=first+ninst);
+    int nbyte=(attr->su_type==SU_INT16)?(2):(4);
     for(int i=0; i<ninst; i++) {
         int64_t f_offs=(first+i)*su->skip+attr->ibyte;
-        int64_t b_offs=i*attr->nbyte;
-        if(pwrite(su->fid, buff+b_offs, attr->nbyte, f_offs)!=attr->nbyte) {
+        int64_t b_offs=i*nbyte;
+        if(pwrite(su->fid, buff+b_offs, nbyte, f_offs)!=nbyte) {
             printf("%s: pwrite is not finished!\n", __func__);
             abort();
         }
@@ -24,16 +25,11 @@ int su_write(SUID_t id, const char *name, void *buff, int first,
         printf("%s: attribute %s not found!\n", __func__, name);
         abort();
     }
-    su_attr_t curr; memcpy(&curr, attr, sizeof(su_attr_t));
-    int length=1;
-    if(curr.ibyte==240) {    //must be trace!
-        length=su->ns; assert(length!=0);
-        curr.nbyte = su->ns*sizeof(float);
-    }
-    void *work = calloc(nmemb, curr.nbyte);
-    su_type2su(buff, curr.db_type, work, curr.su_type, nmemb*length);
-    //memcpy(work, buff, nmemb*length*sizeof(float));
-    su_writebytes(id, &curr, work, first, nmemb);
+    int length=(attr->ibyte==240)?(su->ns):(1);
+    int size=(attr->su_type==SU_INT16)?(2):(4);
+    void *work = calloc(nmemb*length, size);
+    su_type2su(buff, attr->db_type, work, attr->su_type, nmemb*length);
+    su_writebytes(id, attr, work, first, nmemb);
     free(work);
     return 0;
 }
