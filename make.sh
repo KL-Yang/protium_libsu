9d82a95f9c1be86676047c65ce4d914174feab74
yangkunlun <yangkunlun@outlook.com>
2021-02-25 14:12:50 +0800

    update make.sh for test3.c file

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@ protium make
 cd build; rm -rf build; make all; cd ..
 #python3 test.py
 
-flags="--std=gnu99 -I/home/protium/include/ -L/home/protium/lib"
+flags="--std=gnu99 -ggdb -O0 -I/home/protium/include/ -L/home/protium/lib"
 links="build/libsu.a -lptcomm"
 gcc $flags test1.c $links -o test1.exe
 gcc $flags test2.c $links -o test2.exe
-gcc --std=gnu99 -I/home/protium/include/ -L/home/protium/lib t3.c build/libsu.a -lptcomm -o t3.exe
+gcc $flags test3.c $links -o test3.exe 
 
