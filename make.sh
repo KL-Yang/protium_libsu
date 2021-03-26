83255f6c4aa168b5a265beea84144843380a6446
yangkunlun <yangkunlun@gmail.com>
2021-03-26 10:53:39 +0800

    copy lib and header to installation location

--- make.sh
+++ make.sh
@@ -4,6 +4,10 @@ protium make
 cd build; rm -rf build; make all; cd ..
 #python3 test.py
 
+#note: only if success, should do this!!!
+cp libsu.h "$PROTIUM_HOME/include/protium/"
+cp build/libsu.a "$PROTIUM_HOME/lib"
+
 flags="--std=gnu99 -ggdb -O0 -I/home/protium/include/ -L/home/protium/lib"
 links="build/libsu.a -lptcomm"
 gcc $flags test1.c $links -o test1.exe
