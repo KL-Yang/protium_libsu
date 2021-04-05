7a88f0c0cdb378a724fc660ad472647eb28e8a6a
yangkunlun <yangkunlun@gmail.com>
2021-04-05 10:41:43 +0800

    Add installation command

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ cd build; rm -rf build; make all; cd ..
 #python3 test.py
 
 #note: only if success, should do this!!!
+cp build/build/lib.linux-*-3.?/libpysu*.so "$PROTIUM_HOME/pylib/"
 cp libsu.h "$PROTIUM_HOME/include/protium/"
 cp build/libsu.a "$PROTIUM_HOME/lib"
 
